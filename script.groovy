dcb12e0993c2af9df52c08461dc9ceeb5d3506fd
Ilia Javakhishvili <ilo@iMac.local>
2024-03-24 01:14:26 +0100

    call library function in jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-def buildJar (){
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage (){
-    echo "building the docker image ..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
-        sh 'docker build -t ilo2003/testing:jma-2.0 .'
-        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-        sh 'docker push ilo2003/testing:jma-2.0'
-    }
-}
 
 def deployApp (){
     echo 'deploying the application...'

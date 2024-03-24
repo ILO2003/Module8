6ff4fc4009286ef1ec47faa72b8a0c6c217a7f62
ILO2003 <ilkjavaxishvili@gmail.com>
2024-03-24 12:45:51 +0100

    Update script.groovy with my credentials instead of nanas

--- script.groovy
+++ script.groovy
@@ -1,19 +1,18 @@
-def buildJar() {
+def buildJar (){
     echo 'building the application...'
     sh 'mvn package'
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+def buildImage (){
+    echo "building the docker image ..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+    sh 'docker build -t ilo2003/testing:jma-2.0 .'
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'docker push ilo2003/testing:jma-2.0'
     }
 }
 
-def deployApp() {
+def deployApp (){
     echo 'deploying the application...'
 }
-
 return this
